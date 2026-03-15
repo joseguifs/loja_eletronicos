d630fb6c7ccfe88b14f6fb4f5b4c45b0cac747e6
joseguifs <joseguilhermeagente007@gmail.com>
2026-03-15 09:57:57 -0300

    func find para produtos

--- app/models/Produto.php
+++ app/models/Produto.php
@@ -23,4 +23,21 @@ class Produto {
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function find($id)
+    {
+        $stmt = $this->conn->prepare("
+            SELECT 
+                produtos.*,
+                categorias.nome AS categoria_nome,
+                marcas.nome AS marca_nome
+            FROM produtos
+            LEFT JOIN categorias ON produtos.categoria_id = categorias.id
+            LEFT JOIN marcas ON produtos.marca_id = marcas.id
+            WHERE produtos.id = ?
+        ");
+
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
