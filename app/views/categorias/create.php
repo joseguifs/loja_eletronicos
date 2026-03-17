4c2349409da880341c95e258298e6ed08490adce
jcarlos721 <jcarlos721254@gmail.com>
2026-03-17 09:47:48 -0300

    Views adicionadas

---
+++ app/views/categorias/create.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/loja_eletronicos/app/css/style.css">
+
+        <title>Create</title>
+
+    </head>
+    <body>
+
+        <header class="header">
+
+            <div class="titulo">
+                <h1>Nova Categoria</h1>
+            </div>
+
+        </header>
+
+        <main>
+
+            <section class="form">
+                
+                <form class="create-form" method="POST" action="?action=categoria_store">
+
+                    <input 
+                        type="text" 
+                        name="nome" 
+                        placeholder="Nome da categoria"
+                    >
+
+                    <button class="btn-primary" type="submit">
+                        Salvar
+                    </button>
+
+                </form>
+
+            </section>
+
+        </main>
+
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
---
+++ app/views/categorias/get.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="/loja_eletronicos/app/css/style.css">
+
+        <title>Get</title>
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <h1>Detalhes da Categoria</h1>
+
+            <p><strong>ID:</strong> <?= $cat['id'] ?></p>
+            <p><strong>Nome:</strong> <?= $cat['nome'] ?></p>
+
+            <br>
+
+            <button type="button" onclick="window.location.href='?action=categorias'">
+                Voltar
+            </button>
+
+        </div>
+
+    </body>
+
+</html>
